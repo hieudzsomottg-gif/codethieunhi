6687512807879e2c3514be57d71e79a7fe42aa21
hieuhoccode <hieudzsomottg@gmail.com>
2025-11-03 22:31:22 +0700

    Implement even number checker for integer input
    Add a script to print even integers from a given integer n.

---
+++ bài25.py
@@ -0,0 +1,7 @@
+# In các số nguyên chẵn của một số nguyên n
+n = int(input("Nhập số nguyên n là: "))
+while True:
+    if n % 2 == 0:
+        print(i,end="")
+    elif n % 2 != 0:
+        print("Không có sô chẵn nào")
